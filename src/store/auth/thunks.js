1f8937325815bfe246e5c4e3d4b8965133a02de4
Bryan Ron <bryan.ron@hotmail.com>
2022-10-23 22:58:41 -0500

    feat: save google info in store

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { signIngWithGoogle } from "../../firebase/provider";
-import { checkingCredentials } from "./";
+import { checkingCredentials, login, logout } from "./";
 
 export const checkingAuthentication = (email, password) => {
   return async (dispatch) => {
@@ -11,6 +11,8 @@ export const startGoogleSignIn = (email, password) => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
     const result = await signIngWithGoogle();
-    console.log(result);
+    if (!result.ok) dispatch(logout(result.errorMessage));
+    delete result.ok;
+    dispatch(login(result));
   };
 };
