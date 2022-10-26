2f7833e7d0cec7665662f88515904f947d80f1f5
dev <bryan.ron@hotmail.com>
2022-10-25 23:59:57 -0500

    feat: add useCheckAuth custom hook

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,16 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
-import { FirebaseAuth } from "../firebase/config";
+import { useCheckAuth } from "../hooks/useCheckAuth";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { logout, login } from "../store/auth";
 import CheckingAuth from "../ui/components/CheckingAuth";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-      dispatch(login(user));
-    });
-  }, []);
+  const status = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
   }
-
   return (
     <Routes>
       {status === "authenticated" ? (
