69ec1bacbd2343c1335c38ee72c01edb55b78520
dev <bryan.ron@hotmail.com>
2022-10-25 23:25:24 -0500

    feat: add login spinner

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 
-import { AuthRoutes } from '../auth/routes/AuthRoutes';
-import { JournalRoutes } from '../journal/routes/JournalRoutes';
-
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import CheckingAuth from "../ui/components/CheckingAuth";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
+      {/* Login y Registro */}
+      <Route path="/auth/*" element={<AuthRoutes />} />
 
-        {/* Login y Registro */}
-        <Route path="/auth/*" element={ <AuthRoutes /> } />
-
-        {/* JournalApp */}
-        <Route path="/*" element={ <JournalRoutes /> } />
-
+      {/* JournalApp */}
+      <Route path="/*" element={<JournalRoutes />} />
     </Routes>
-  )
-}
+  );
+};
