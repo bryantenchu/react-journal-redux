23de585778df60f10c865ec954312ff84ea77bf8
dev <bryan.ron@hotmail.com>
2022-10-26 23:06:41 -0500

    feat: added journal slice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const journalSlice = createSlice({
+  name: "journal",
+  initialState: {
+    isSaving: true,
+    messageSaved: "",
+    notes: [],
+    active: null,
+  },
+  reducers: {
+    addNewEmptyNote: (state, action) => {},
+    setActiveNote: (state, action) => {},
+    setNotes: (state, action) => {},
+    setSaving: (state, action) => {},
+    updateNote: (state, action) => {},
+    deleteNodeById: (state, action) => {},
+  },
+});
+
+export const {
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNodeById,
+} = journalSlice.actions;
