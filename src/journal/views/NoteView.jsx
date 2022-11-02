795719a84ece368bb9136452501e321b00093045
Bryan Ron <bryan.ron@hotmail.com>
2022-11-01 23:11:04 -0500

    feat: add file input

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,28 +1,31 @@
-import {SaveOutlined} from "@mui/icons-material";
-import {Button, Grid, TextField, Typography} from "@mui/material";
+import {SaveOutlined, UploadFile} from "@mui/icons-material";
+import {Button, Grid, IconButton, TextField, Typography} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {ImageGallery} from "../components";
 import {useForm} from "../../hooks/useForm";
-import {useEffect, useMemo} from "react";
+import {useEffect, useMemo, useRef} from "react";
 import {setActiveNote, startSaveNote} from "../../store/journal";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.css"
 
 export const NoteView = () => {
     const dispatch = useDispatch()
-    const {active: note, messageSaved,isSaving} = useSelector((state) => state.journal);
+    const {active: note, messageSaved, isSaving} = useSelector((state) => state.journal);
     const {body, title, date, onInputChange, formState} = useForm(note)
+
     const dateString = useMemo(() => {
         return new Date(date).toUTCString()
     }, [date])
+
+    const fileInputRef = useRef()
 
     useEffect(() => {
         dispatch(setActiveNote(formState))
     }, [formState])
 
     useEffect(() => {
-        if(messageSaved.length>0) {
-            Swal.fire('Nota actualizada',messageSaved,'success')
+        if (messageSaved.length > 0) {
+            Swal.fire('Nota actualizada', messageSaved, 'success')
         }
     }, [messageSaved])
 
@@ -30,6 +33,11 @@ export const NoteView = () => {
         dispatch(startSaveNote());
     }
 
+    const onFileInputChange = ({target}) => {
+        if (target.files === 0) return
+        console.log('subiendo archivos')
+        // dispatch(startUploadingFiles(target.files))
+    }
     return (
         <Grid
             className="animate__animated animate__fadeIn animate__faster"
@@ -45,6 +53,10 @@ export const NoteView = () => {
                 </Typography>
             </Grid>
             <Grid item>
+                <input type='file'  ref={fileInputRef} multiple onChange={onFileInputChange} style={{display:'none'}}/>
+                <IconButton color='primary' disabled={isSaving} onClick={()=>fileInputRef.current.click()}>
+                    <UploadFile/>
+                </IconButton>
                 <Button disabled={isSaving} onClick={onSaveNote} color="primary" sx={{padding: 2}}>
                     <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
                     Guardar
